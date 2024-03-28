cc586e39e43ca8cf64af5e294cbebd8754f8d77f
Dawid Roszman <dawidroszman@gmail.com>
2024-03-28 18:58:55 +0100

    Added error and loading handling

--- src/components/ListOfTags.tsx
+++ src/components/ListOfTags.tsx
@@ -1,12 +1,13 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useTags } from './TagsContext';
-import { useQuery } from '@tanstack/react-query';
-import { fetchTags } from '../utils/queries';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useTags } from "./TagsContext";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchTags } from "../utils/queries";
+import { Alert, Stack, Skeleton } from "@mui/material";
 
 interface Row {
   name: string;
@@ -19,57 +20,71 @@ function CustomPaginationActionsTable() {
   const { page, rowsPerPage } = tagsSettings;
 
   const fetchTagsHelper = async () => {
-    console.log(page, rowsPerPage)
+    console.log(page, rowsPerPage);
     const data = await fetchTags(page, rowsPerPage);
     return data;
-  }
+  };
 
   const query = useQuery({
-    queryKey: ['tags'],
-    queryFn: fetchTagsHelper
+    queryKey: ["tags", page, rowsPerPage],
+    queryFn: fetchTagsHelper,
   });
 
-  if (query.isPending) return <div>Loading...</div>
-  if (query.isError) return <div>Error: {query.error.message}</div>
-  console.log(query.data);
-  const rows = query.data.data.items.map((item: any) => {
+  if (query.isPending)
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 500 }} aria-label="loading for table">
+          <TableBody>
+            <TableRow>
+              <TableCell>Tag</TableCell>
+              <TableCell>Count</TableCell>
+            </TableRow>
+            {Array(rowsPerPage).fill(0).map((_, id) => (
+              <TableRow key={id}>
+                <TableCell component="th" scope="row">
+                  <Skeleton />
+                </TableCell>
+                <TableCell style={{ width: 160 }}>
+                  <Skeleton />
+                </TableCell>
+              </TableRow>
+            )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  if (query.isError)
+    return (
+      <Alert severity="error">
+        Error: {query.error.message}. Please try again later!
+      </Alert>
+    );
+  const rows = query.data.items.map((item: any) => {
     return {
       name: item.name,
-      count: item.count
-    }
-  })
-  const has_more = query.data.data.has_more;
-
-
-
+      count: item.count,
+    };
+  });
+  const has_more = query.data.has_more;
 
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-
         <TableBody>
           <TableRow>
             <TableCell>Tag</TableCell>
             <TableCell>Count</TableCell>
           </TableRow>
-          {(tagsSettings.rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map((row: Row) => (
+          {rows.map((row: Row) => (
             <TableRow key={row.name}>
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell style={{ width: 160 }}>
-                {row.count}
-              </TableCell>
+              <TableCell style={{ width: 160 }}>{row.count}</TableCell>
             </TableRow>
-          ))}
-          {/* {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )} */}
+          )
+          )}
         </TableBody>
       </Table>
     </TableContainer>
@@ -80,6 +95,7 @@ const ListOfTags = () => {
     <>
       <CustomPaginationActionsTable />
     </>
-  )
-}
-export default ListOfTags
+  );
+};
+export default ListOfTags;
+
--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,10 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
-export const fetchTags = async (numOfRows: number, page: number) => {
-  try {
-    const response = await axios.get(`https://api.stackexchange.com/2.3/tags?page=${page}&pagesize=${numOfRows}&order=desc&sort=popular&site=stackoverflow&filter=!T.BkwE7kN.OD6_qxzS`)
-    return { data: response.data, error: null };
-  } catch (error: any) {
-    return { data: null, error: error.message };
-  }
-}
+export const fetchTags = async (page: number, numOfRows: number) => {
+  const response = await axios.get(
+    `htts://api.stackexchange.com/2.3/tags?page=${page}&pagesize=${numOfRows}&order=desc&sort=popular&site=stackoverflow&filter=!T.BkwE7kN.OD6_qxzS`,
+  );
+  return response.data;
+};
